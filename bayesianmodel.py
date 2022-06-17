c025ded94e6153d88d61f17aed9aa3c63b4a74ad
priya <priyanka.arathi@gmail.com>
2022-06-17 08:32:18 +0200

    initial commit

--- bayesianmodel.py
+++ bayesianmodel.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 from blitz.modules import BayesianLinear
 from blitz.utils import variational_estimator
+import xlsxwriter
 
 # device config
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -13,10 +14,11 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Hyper parameter
 # Number of days data to predict one day in future
 input_sz = 30
-h1_size = 500
-lr = 0.01
-num_epochs = 1000
-test_ens = 20
+
+# Need to tune these
+h1_size = 250 #100,250,500
+lr = 0.01 # 0.1, 0.01, 0.001
+num_epochs = 1000 #500, 1000, 1500
 
 #Step 1: Preparing dataset
 # load train dataset
@@ -78,9 +80,10 @@ x_test = torch.from_numpy(x_test.astype(np.float32))
 class BNNetwork(nn.Module):
     def __init__(self, input_size, hidden_size):
         super(BNNetwork, self).__init__()
-        self.bl1 = BayesianLinear(input_size, hidden_size)
+        self.bl1 = BayesianLinear(input_size, hidden_size, bias=True)
         self.relu = nn.ReLU()
         self.bl2 = BayesianLinear(hidden_size, 1)
+
 
     def forward(self, x):
         out = self.bl1(x)
@@ -96,6 +99,8 @@ criterion = nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=lr)
 
 # step 4: training
+MSE_err_test_data = []
+MAPE_err_test_data = []
 for epoch in range(num_epochs):
     # forward pass and loss
     #y_pred = model(x_train)
@@ -116,16 +121,50 @@ for epoch in range(num_epochs):
     if (epoch + 1) % 100 == 0:
         print(f'epoch: {epoch + 1}, loss= {loss.item():.4f}')
 
-with torch.no_grad():
-    print('testing the results')
-    y_pred = model(x_test)
-    MSE_err = ((y_actual-y_pred) ** 2 ).mean()
-    print(f'MSE error = {MSE_err:.4f}')
+        with torch.no_grad():
+            print('testing the results')
+            preds = [model(x_test) for i in range(100)]
+            preds = torch.stack(preds)
+            means = preds.mean(axis=0)
+            stds = preds.std(axis=0)
+            ci_upper = means + (3 * stds)  # uncertainty information
+            ci_lower = means - (3 * stds)
+            ci_lower = ci_lower.reshape(-1,)
+            ci_upper = ci_upper.reshape(-1, )
+            MSE_err = ((y_actual-means) ** 2 ).mean()
+            MSE_err_test_data.append(MSE_err)
+            MAPE_err = (abs(y_actual-means)/y_actual).mean() * 100
+            MAPE_err_test_data.append(MAPE_err)
+            print(f'MSE error = {MSE_err:.8f}')
+            print(f'MAPE error = {MAPE_err:.8f}')
+
+# new metric to check how often the data point lies with the uncertainity range
+accuracy_within_ci = 0
+for i in range(len(y_actual)):
+    if (y_actual[i] <= ci_upper[i] and y_actual[i] >= ci_lower[i]):
+        accuracy_within_ci = accuracy_within_ci + 1
+
+confidence_interval_lower_limit = ((ci_lower-y_actual)/y_actual*100).mean()
+confidence_interval_upper_limit = ((ci_upper-y_actual)/y_actual*100).mean()
+print(f'### The accuracy % is {accuracy_within_ci/len(y_actual)*100}% ###')
+print(f'lower bound is {confidence_interval_lower_limit}% and upper bound is {confidence_interval_upper_limit}%')
 
 #Plot test
-plt.plot(y_actual, color='blue', label=f"Actual Apple Price")
-plt.plot(y_pred, color='red', label=f"Predicted Apple Price")
+x = np.arange(1, len(y_actual)+1)
+plt.subplot(1,2,1)
+plt.plot(x, y_actual, color='blue', label=f"Actual Apple Price")
+plt.plot(x, means, color='red', label=f"Predicted Apple Price")
+plt.plot(x, ci_upper, color='lightpink')
+plt.plot(x, ci_lower, color='lightpink')
+plt.fill_between(x, ci_lower, ci_upper, color="pink")
 plt.xlabel('Days')
 plt.ylabel("share price")
+plt.title("Predicted & Actual Share Price")
 plt.legend()
+
+plt.subplot(1,2,2)
+plt.plot(MSE_err_test_data, color ='green')
+plt.xlabel('Every 100th epoch')
+plt.ylabel("MSE error")
+plt.title("MSE error for training data")
 plt.show()
